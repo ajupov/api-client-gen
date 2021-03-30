1dd621368de06457c0c3b646e6688a1a553d4025
Usman Ayupov <u.ayupov@dodopizza.com>
2021-03-30 11:18:09 +0300

    Fix swagger

--- parser/types/SwaggerComponentsSchemas.go
+++ parser/types/SwaggerComponentsSchemas.go
@@ -1,12 +1,12 @@
 package parser
 
 type SwaggerComponentsSchemas map[string]struct {
-	Enum                 *[]int                              `json:"enum,omitempty"`
 	Type                 *SwaggerSchemaType                  `json:"type,omitempty"`
-	Required             *[]string                           `json:"required,omitempty"`
-	Description          *string                             `json:"description,omitempty"`
 	Format               *string                             `json:"format,omitempty"`
+	Description          *string                             `json:"description,omitempty"`
+	Enum                 *[]int                              `json:"enum,omitempty"`
 	XEnumNames           *[]string                           `json:"x-enumNames,omitempty"`
 	Properties           *SwaggerComponentsSchemasProperties `json:"properties,omitempty"`
 	AdditionalProperties *bool                               `json:"additionalProperties,omitempty"`
+	Required             *[]string                           `json:"required,omitempty"`
 }
